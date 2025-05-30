86b9290d98ac3da068de7e0753584f16a4a91fad
masashimorita <masashi.morita.mm@gmail.com>
2025-05-30 19:28:38 +0900

    :recycle: Allow lazy store initialization via setStore function

--- src/routember.ts
+++ src/routember.ts
@@ -2,16 +2,23 @@ import { RouteStore } from './stores';
 
 const DEFAULT_EXCLUDE_PATHS = ['/login', '/sign-in', '/sign-up', '/signup', '/forgot-password', '/reset-password', '/logout', '/api/'];
 
-export function useRoutember(store: RouteStore, excludePaths: string[] = DEFAULT_EXCLUDE_PATHS) {
-  let currentStore: RouteStore = store;
+export function useRoutember(store: RouteStore | null = null, excludePaths: string[] = DEFAULT_EXCLUDE_PATHS) {
+  let currentStore: RouteStore | null = store;
 
+  const setStore = (store: RouteStore) => {
+    currentStore = store;
+  };
+  
   const setRedirectUrl = async (url: string) => {
+    if (!currentStore) throw new Error('Store is not set');
     if (excludePaths.some(path => url.includes(path))) return;
 
     await currentStore.save(url);
   };
 
   const getRedirectUrl = async () => {
+    if (!currentStore) throw new Error('Store is not set');
+
     const redirectUrl: string | null = await currentStore.get();
     await currentStore.clear();
     return redirectUrl;
@@ -22,5 +29,5 @@ export function useRoutember(store: RouteStore, excludePaths: string[] = DEFAULT
     router.replace(redirectUrl || defaultUrl);
   };
 
-  return { setRedirectUrl, getRedirectUrl, redirectRememberedUrl };
+  return { setStore, setRedirectUrl, getRedirectUrl, redirectRememberedUrl };
 }
